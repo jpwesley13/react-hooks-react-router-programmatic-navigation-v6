204af9edfb5da52e4cbeaae95da3fa23b89a8fd4
Jared Patterson <jpwesley34@gmail.com>
2024-02-15 20:35:07 -0800

    Follows along.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const navigate = useNavigate()
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const login = () =>{
+    setIsLoggedIn(true);
+  }
+
+  const logout = () =>{
+    setIsLoggedIn(false);
+  };
+
+    // Add programmatic navigation for login and logout
+  useEffect(() =>{
+    if (isLoggedIn) {
+        // navigates to Home route if user is logged in
+      navigate("/");
+    } else {
+        // navigates to Login route if user is logged out
+      navigate("/login");
+    };
+  }, [isLoggedIn]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+{/* Add conditional rendering so users have to be logged in to see pages on the site */}
+      {isLoggedIn ? <NavBar logout={logout}  /> : <Navigate to="/login" />}
+      <Outlet context={login}/>
     </div>
   );
-}
+};
 
 export default App;
